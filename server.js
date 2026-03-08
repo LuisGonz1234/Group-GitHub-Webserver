85b922c1d06d345b707b18068e8084cd9b0e1b4e
shaynyman <shay.nyman@southeasttech.edu>
2026-03-07 22:04:12 -0800

    Update server.js
    updated 404 to go to 404.html page

--- server.js
+++ server.js
@@ -38,11 +38,15 @@ const server = http.createServer((req, res) => {
   } 
   else {
 
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 - Page Not Found");
+    const filePath = path.join(__dirname, "404.html");
 
-  }
+    fs.readFile(filePath, "utf8", (err, data) => {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end(data);
+  });
 
+ }
+  
 });
 
 server.listen(PORT, () => {
